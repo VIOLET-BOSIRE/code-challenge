6f6d0e6e296d3354d5c00b729c4b229077da7644
“VIOLET-BOSIRE” <violet.bosire@student.moringaschool.com>
2022-11-28 15:16:39 +0300

    added a function

--- grade.js
+++ grade.js
@@ -4,10 +4,10 @@ function gradingSystem(grade){
     if (grade >= 80 && grade <= 100){
         console.log("A")
 
-   /* }else if (grade >= 60 && grade<=79){
+    }else if (grade >= 60 && grade<=79){
         console.log("B")
 
-    }else if (grade >= 49 && grade <=59){
+    /*}else if (grade >= 49 && grade <=59){
         console.log("C")
 
     }else if (grade < 40){
