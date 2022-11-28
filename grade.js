cfee4198f18483d1345ac0daa61090fae82615cc
“VIOLET-BOSIRE” <violet.bosire@student.moringaschool.com>
2022-11-28 15:19:20 +0300

    added the if else statement in the function

--- grade.js
+++ grade.js
@@ -7,10 +7,10 @@ function gradingSystem(grade){
     }else if (grade >= 60 && grade<=79){
         console.log("B")
 
-    /*}else if (grade >= 49 && grade <=59){
+    }else if (grade >= 49 && grade <=59){
         console.log("C")
 
-    }else if (grade < 40){
+    }else if (grade < 40&& grade>=0){
         console.log("E")
     }
     else
