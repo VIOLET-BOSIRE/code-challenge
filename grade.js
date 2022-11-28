83d6abd28ff2e022ec8a3b95e76c797f2cbc9d24
“VIOLET-BOSIRE” <violet.bosire@student.moringaschool.com>
2022-11-28 15:14:58 +0300

    added the three js files

---
+++ grade.js
@@ -0,0 +1,22 @@
+// a program that grades ones marks
+function gradingSystem(grade){
+
+    if (grade >= 80 && grade <= 100){
+        console.log("A")
+
+   /* }else if (grade >= 60 && grade<=79){
+        console.log("B")
+
+    }else if (grade >= 49 && grade <=59){
+        console.log("C")
+
+    }else if (grade < 40){
+        console.log("E")
+    }
+    else
+
+    {
+        console.log("invalid grade")
+    }
+}
+console.log(gradingSystem(100))
